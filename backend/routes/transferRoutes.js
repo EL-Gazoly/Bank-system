898eb021e418df353d637d5e67b4621e11876671
Adham ElGazoly <elgazoly11@gmail.com>
2022-05-03 02:59:34 +0200

    Backend

--- backend/routes/transferRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-const {updataeBalance} = require('../controller/transferController')
-router.get('/:senderid/:id/:amount',updataeBalance)
-
-module.exports = router
