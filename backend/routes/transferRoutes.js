23f0225cfbee2bc06478dc691633522ced75bb43
Adham ElGazoly <elgazoly11@gmail.com>
2022-05-16 14:25:52 +0200

    Delete backend directory

--- backend/routes/transferRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-const {updataeBalance ,showTable} = require('../controller/transferController')
-router.put('/:senderid/:id/:amount',updataeBalance)
-
-
-router.get('/table',showTable)
-module.exports = router    
