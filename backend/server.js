ef7e8b8a09bff76b7a11b52d92435a8c454ed56f
Adham ElGazoly <adham20191701021@cis.asu.edu.eg>
2022-05-19 20:40:02 +0200

    Docker

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,6 @@ connectDB()
 app.use('/api/users',require('./routes/userRoutes'))
 app.use('/api/transfer',require('./routes/transferRoutes'))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  )
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'))
-}
  
-app.listen(process.env.PORT || 8080)
+app.listen(5000)
