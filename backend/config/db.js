8a3a6ce6299cec6e98d876db45a7517657fe7853
Adham ElGazoly <elgazoly11@gmail.com>
2022-05-31 18:11:36 +0200

    Back-end

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://elgazoly:elgazoly11@projectscluster.zh7z2.mongodb.net/bankSystem?retryWrites=true&w=majority" , {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
